d93ef2a3767d0fc9e0319dfcd7395ad24e8b64f7
Bart Kooijman <bkooijman@anwb.nl>
2021-10-21 16:50:55 +0200

    added companies page

--- CMS/src/Foundation/WildCardItemResolver/code/Resolvers/WildCardItemResolver.cs
+++ CMS/src/Foundation/WildCardItemResolver/code/Resolvers/WildCardItemResolver.cs
@@ -30,7 +30,12 @@ namespace Jobify.Foundation.WildCardItemResolver.Resolvers
             if (newContextItem != null)
             {
                 Context.Item = newContextItem;
+                return;
             }
+
+            //TODO: Handle 404 to override wildcard resolving
+
+            return;
         }
 
         public static Item GetContextItemByWildCard(string urlPath)
@@ -69,6 +74,7 @@ namespace Jobify.Foundation.WildCardItemResolver.Resolvers
                         newContextItem = results.First().GetItem();
                         return newContextItem;
                     }
+
                     return null;
                 }
             }
