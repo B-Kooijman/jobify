5546edea2b668de9a83af03f198145013872fdf6
Bart Kooijman <32140599+B-Kooijman@users.noreply.github.com>
2022-05-10 17:31:32 +0200

    Feature/monorepo components (#16)
    * added sample component
    
    * removed disconnected mode
    
    * refactored export
    
    Co-authored-by: Bart Kooijman <bkooijman@anwb.nl>

--- frontend/packages/core/src/index.tsx
+++ frontend/packages/core/src/index.tsx
@@ -1,1 +1,2 @@
-export { Button, type ButtonProps } from "./Button";
+export * from "./atoms";
+export * from "./molecules";
--- frontend/packages/nextjs/src/components/ContentBlock.tsx
+++ frontend/packages/nextjs/src/components/ContentBlock.tsx
@@ -1,24 +1,3 @@
-import { Text, RichText, Field } from '@sitecore-jss/sitecore-jss-nextjs';
-import { IComponentProps } from 'lib/component-props';
-
-type ContentBlockProps = IComponentProps & {
-  fields: {
-    heading: Field<string>;
-    content: Field<string>;
-  };
-};
-
-/**
- * A simple Content Block component, with a heading and rich text block.
- * This is the most basic building block of a content site, and the most basic
- * JSS component that's useful.
- */
-const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => (
-  <>
-    <Text tag="h2" className="display-4" field={fields.heading} />
-
-    <RichText className="contentDescription" field={fields.content} />
-  </>
-);
+import { ContentBlock } from "@jobify/core";
 
 export default ContentBlock;
